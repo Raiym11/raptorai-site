318d2de70e9373cb4262f17e025905136515e67e
Raiymbek <137402101+Raiym11@users.noreply.github.com>
2026-09-04 15:03:51 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -29,11 +29,11 @@ const securityHeaders = [
     key: "Content-Security-Policy",
     value: [
       "default-src 'self'",
-      "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
+      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://mc.yandex.ru",
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdnjs.cloudflare.com",
       "font-src 'self' https://fonts.gstatic.com https://cdnjs.cloudflare.com",
       "img-src 'self' data: https:",
-      "connect-src 'self' https://n8n.raptorai.ru",
+     "connect-src 'self' https://n8n.raptorai.ru https://mc.yandex.ru",
       "frame-src 'self'",
       "object-src 'none'",
       "base-uri 'self'",
